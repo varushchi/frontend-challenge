d2621a50d17496897b66ff2fb9b511bb77ec4128
varushchi <vadim.shchigolev@mail.ru>
2025-01-20 20:45:36 +0300

    infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Card from './components/Card'
 import Header from './components/Header'
 
@@ -26,26 +26,58 @@ function App() {
     }]
   }
 
-  const [cats, setCats] = useState<APIResponse[] | null>()
-  const [likedCats, setLikedCats] = useState<{id: string, url: string, isLiked?: true}[] | undefined>(JSON.parse(localStorage.getItem('likedCats') || '""') !== null ? JSON.parse(localStorage.getItem('likedCats') || '""') : [])
+  const [cats, setCats] = useState<APIResponse[]>([])
+  const [likedCats, setLikedCats] = useState<{id: string, url: string, isLiked?: true}[]>(JSON.parse(localStorage.getItem('likedCats') || '""') !== null ? JSON.parse(localStorage.getItem('likedCats') || '""') : [])
   const [page, setPage] = useState<'All' | 'Liked'>('All')
+  const [APIPage, setAPIPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+
+  async function fetchCats(page: number){
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=20&page=${page}&api_key=${import.meta.env.VITE_CAT_API_KEY}`)
+    const data = await response.json()
+    return data
+  }
+
+  async function loadCats(){
+    setLoading(true)
+    const newCats = await fetchCats(APIPage)
+    setLoading(false)
+    setCats([...cats, ...newCats])
+    setLoading(false)
+  }
 
   useEffect(() => {
-    
-    async function getCats() {
-      const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=${import.meta.env.CAT_API_KEY}`)
-      if(res.status === 200){
-        const data = await res.json()
-        setCats(data)
-      }
-    }
-    getCats()
-  }, [])
+    loadCats()
+  }, [APIPage])
+
+  
 
   useEffect(() => {
     localStorage.setItem('likedCats', JSON.stringify(likedCats))
-    console.log('sdsdfsdfsfsfsf')
   },[likedCats])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !loading) {
+          setAPIPage(APIPage + 1)
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current)
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current)
+      }
+    }
+  }, [loading])
 
 
   const catElem = cats && cats.map(elem =>{
@@ -99,10 +131,14 @@ function App() {
         w-full
         mx-auto
         justify-center
+        items-center
         gap-[48px]
       `}>
-        {(cats && catElem) && page === "All" ? catElem : (likedCatsElem && likedCatsElem.length > 0 ? likedCatsElem : <p> любимых котиков нет</p>)}
+        {page === 'All' && cats && catElem}
+        {page === 'Liked' && likedCatsElem}
+        <div ref={observerRef}></div>
       </div>
+      <div className='text-[14px] w-[195px] mx-auto'>{loading && <p>... загружаем еще котиков ...</p>}</div>
     </div>
   )
 }
