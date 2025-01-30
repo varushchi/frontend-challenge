72d78cbebb96c201818f9bfc5c197e08b87e248c
varushchi <vadim.shchigolev#mail.ru>
2025-01-30 16:38:02 +0300

    new image load

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,47 +1,45 @@
-import { useEffect, useState } from 'react'
-import filledHeart from '../assets/filled_heart.svg'
-import hollowedHeart from '../assets/hollow_heart.svg'
-import hoveredHeart from '../assets/hovered_heart.svg'
+import { useEffect, useState } from "react"
+import filledHeart from "../assets/filled_heart.svg"
+import hollowedHeart from "../assets/hollow_heart.svg"
+import hoveredHeart from "../assets/hovered_heart.svg"
 
-interface Props{
+interface Props {
   id: string
   img: string
   isLiked: boolean
-  handleLike : (e: React.MouseEvent<HTMLButtonElement>) => void
+  handleLike: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export default function Card(props: Props) {
-  
-  const [imageSrc, setImageSrc] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [imageSrc, setImageSrc] = useState<string | undefined>(undefined)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>
 
-    async function fetchImg() {
-      setLoading(true)
-      try{
-        const res = await fetch(props.img, {
-          method: 'GET',
-          mode: 'no-cors',
-          signal: AbortSignal.timeout(5000),
-          headers: {
-            'Content-Type': 'image/jpeg',
-          }
-        })
-        if (res.ok){
-          setImageSrc(props.img)
-        } else throw new Error('Img didnt load')
-      } catch(error) {
-        console.log('fsdfsdfsdfsf')
-        console.error(error)
-        setImageSrc('')
-      } finally{
-        setLoading(false)
-      }
-    }
+    const img = new Image()
+    img.src = props.img
+    
+    img.onload = () => {
+      setImageSrc(props.img)
+      setLoading(false)
+      clearTimeout(timeoutId)
+    };
 
-    fetchImg()
+    img.onerror = () => {
+      setImageSrc(undefined)
+      setLoading(false)
+      clearTimeout(timeoutId)
+    };
 
+    
+    timeoutId = setTimeout(() => {
+      console.error("Image load timeout:", props.img)
+      setImageSrc(undefined)
+      setLoading(false)
+    }, 10000)
+
+    return () => clearTimeout(timeoutId)
   }, [])
 
 
@@ -52,7 +50,15 @@ export default function Card(props: Props) {
       hover:outline outline-[30px] outline-white 
       hover:shadow-[0px_5px_5px_32px_rgba(0,0,0,0.3)]'>
       
-      {loading ? <p className='text-[14px] w-fill text-center'>...загружаем картинку...</p> : <img src={imageSrc} alt={props.id} className="w-full h-full object-cover"/>}
+      {loading ?
+        <p className='text-[14px] w-fill text-center'>...загружаем картинку...</p> :
+        <img
+          src={imageSrc}
+          loading='lazy'
+          alt={props.id}
+          className="w-full h-full object-cover"
+        />
+      }
       <div
         className='absolute bottom-0 
         h-[64px] w-full 
